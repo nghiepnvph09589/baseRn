3d1ffbafa90b18379094429a2a1328274aea504e
nghiepnv <nghiepnv2007@gmail.com>
2022-05-30 11:27:54 +0700

    comit'

--- BaseRN/babel.config copy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
-    },
-  },
-  plugins: ['react-native-reanimated/plugin'],
-}
